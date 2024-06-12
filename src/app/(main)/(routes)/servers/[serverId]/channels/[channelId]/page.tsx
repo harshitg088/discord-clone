3e7a49754d0991050f2c5afb96d9300788340ff6
harshitg088 <harshitg088@gmail.com>
2024-06-13 01:13:39 +0530

    feat: implement messages API

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -45,7 +45,7 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
       <div className="flex-1"> Future Message</div>
       <ChatInput
         name={channel.name}
-        apiUrl="api/socket/messages"
+        apiUrl="/api/socket/messages"
         type="channel"
         query={{ channelId: channel.id, serverId: channel.serverId }}
       />
