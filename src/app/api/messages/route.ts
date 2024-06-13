15ca5f553c313bfd488c3460f430ec2f01302ea5
harshitg088 <harshitg088@gmail.com>
2024-06-14 02:13:00 +0530

    feat: add chat messages component

---
+++ src/app/api/messages/route.ts
@@ -0,0 +1,73 @@
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { Message } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+    const cursor = searchParams.get('cursor');
+    const channelId = searchParams.get('channelId');
+    const MESSAGE_BATCH = 10;
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!channelId) {
+      return new NextResponse('Channel Id missing', { status: 400 });
+    }
+
+    let messages: Message[] = [];
+
+    if (Number(cursor) > 0) {
+      messages = await db.message.findMany({
+        take: MESSAGE_BATCH,
+        skip: 1,
+        cursor: {
+          id: cursor as string,
+        },
+        where: {
+          channelId: channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+    } else {
+      messages = await db.message.findMany({
+        take: MESSAGE_BATCH,
+        where: {
+          channelId: channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+    }
+    let nextCursor = null;
+    if (messages.length === MESSAGE_BATCH) {
+      nextCursor = messages[MESSAGE_BATCH - 1].id;
+    }
+
+    return NextResponse.json({ items: messages, nextCursor });
+  } catch (err) {
+    console.log('[MESSAGE_GET]', err);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -1,14 +1,15 @@
 'use client';
+import EmojiPicker from '@/components/emoji-picker';
+import { Input } from '@/components/ui/input';
 import { useModal } from '@/hooks/use-modal-store';
 import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
 import { Plus } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import qs from 'query-string';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
-import EmojiPicker from '../emoji-picker';
 import { Form, FormControl, FormField, FormItem } from '../ui/form';
-import { Input } from '../ui/input';
 
 interface ChatInputProps {
   apiUrl: string;
@@ -23,6 +24,7 @@ const formSchema = z.object({
 
 const ChatInput = ({ name, apiUrl, query, type }: ChatInputProps) => {
   const { onOpen } = useModal();
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -39,6 +41,8 @@ const ChatInput = ({ name, apiUrl, query, type }: ChatInputProps) => {
         query,
       });
       await axios.post(url, values);
+      form.reset();
+      router.refresh();
     } catch (error) {
       console.log(error);
     }
